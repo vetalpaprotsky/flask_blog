3b21c15f3d213487d4f2b1bf031a826525dadb87
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2020-09-27 17:11:14 +0300

    add templates

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Vitalii Paprotskyi',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018',
+    },
+    {
+        'author': 'Foo Bar',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018',
+    },
+]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Home Page</h1>'
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
+    return render_template('about.html', title='About')
