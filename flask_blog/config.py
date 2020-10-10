b24a3afa53f5b566ebcc2a925da887181e885abb
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2020-10-10 12:52:36 +0300

    use config file instead of env vars

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -1,11 +1,15 @@
-import os
+import json
+
+
+with open('config.json') as condig_file:
+    config = json.load(condig_file)
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = config.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = config.get('MAIL_USERNAME')
+    MAIL_PASSWORD = config.get('MAIL_PASSWORD')
